d9e582716edda0a4117211617452237912c63573
崮生 <2234839456@qq.com>
2020-07-18 21:05:53 +0800

    分离range组件，可以精确输入

---
+++ src/base/select/Range.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let value;
+  export let min;
+  export let max;
+  /** boolean 控制是否显示精确数值输入框 */
+  export let showInput = false;
+
+  function onInput(e) {
+    setTimeout(() => {
+      if (value > max) {
+        value = max;
+      } else if (value < min) {
+        value = min;
+      } else {
+      }
+    });
+  }
+</script>
+
+<input bind:value={value} {min} {max} type="range"/>
+{#if showInput}
+  <input on:input={onInput} bind:value={value} {min} {max} type="number" placeholder="min:{min} max:{max}" />
+{/if}
