cd9b143ee667fde6dcb59889f2bd6065d141bf5c
崮生 <2234839456@qq.com>
2020-07-18 17:07:12 +0800

    border 新增旋转功能

--- src/用例/border.svelte
+++ src/用例/border.svelte
@@ -9,8 +9,10 @@
 	  topWidth: { default: 20, type: "range", max: 100, min: 0 },
 	  rightWidth: { default: 20, type: "range", max: 100, min: 0 },
 	  bottomWidth: { default: 20, type: "range", max: 100, min: 0 },
-	  leftWidth: { default: 20, type: "range", max: 100, min: 0 }
+	  leftWidth: { default: 20, type: "range", max: 100, min: 0 },
+	  rotateZ: { default: 45, type: "range", max: 360, min: 0 }
 	};
+	//     transform: rotateZ(45deg)
 </script> 
 <div> 
 	<ValueSwitcher {config} let:r={s} >
@@ -19,7 +21,7 @@
 								border-right:{s.rightWidth}px solid yellow;
 								border-bottom:{s.bottomWidth}px solid black;
 								border-left:{s.leftWidth}px solid blue;
-								
+								transform: rotateZ({s.rotateZ}deg);
 								">
 			
 		</div>
