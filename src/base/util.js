23e8bdcbe921ee921f94a5f0e5a2f5be6f56f60f
崮生 <2234839456@qq.com>
2020-07-18 15:37:53 +0800

    引入LZString库用来压缩字符串

--- src/base/util.js
+++ src/base/util.js
@@ -1,5 +1,16 @@
+import lz_string from "lz-string";
+
 export const value_map = {
   "justify-content":
     "start,flex-start,flex-end,center,left,right,baseline,first baseline,last baseline,space-between,space-around,space-evenly,stretch,safe,unsafe",
   "mask-mode": `alpha,luminance,match-source,inherit,initial,unset`
+};
+
+export const LZString = {
+  compressToEncodedURIComponent(str) {
+    return lz_string.compressToEncodedURIComponent(str);
+  },
+  decompressFromEncodedURIComponent(str) {
+    return lz_string.decompressFromEncodedURIComponent(str);
+  }
 };
